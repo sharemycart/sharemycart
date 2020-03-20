2d374a0ba7c6c6c5469d09f544ad68947f27016e
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-03-20 22:02:27 +0100

    Implemented a first test for adding a need

--- src/backend/tests/e2e/shoppingLists.js
+++ src/backend/tests/e2e/shoppingLists.js
@@ -6,6 +6,7 @@ const request = supertest(process.env.SERVER_URL || 'http://localhost:4010')
 describe('GET /shoppinglists', () => {
   it('should return an array of objects', async () => {
     const result = await request.get('/shoppinglists')
+    result.status.should.equal(200)
     result.body.should.be.Array
     result.body.length.should.be.greaterThan(0)
     result.body[0].should.be.an.Object
@@ -73,9 +74,28 @@ describe('DELETE /shoppinglists/{id}/items/{item_id}', () => {
 })
 
 describe('POST /shoppinglists/{id}/items/{item_id}/need', () => {
-  it('should increase the amount of the specified item')
+  it('should increase the amount of the specified item', async () => {
+    const listResponse = await request.post('/shoppinglists')
+      .send({
+        name: 'Baby Stuff',
+        type: 'shopping',
+        isDefault: false,
+        items: [{name: 'Pampers', amount: 2000, unit: 'pc'}]
+      })
+    listResponse.status.should.equal(200)
+    const {id, items: [{itemId}]} = listResponse.body
+    
+    const needResponse = await request.post(`/shoppinglists/${id}/items/${itemId}/need`)
+      .send({amount: 500})
+    needResponse.status.should(201)
+
+    const itemResponse = await request.get(`/shoppinglists/${id}`)
+    itemResponse.body.items[0].amount.should.equal(2500)
+  })
+
+  it('should report 400 if the it in the path is not a valid uuid')
 
   it('should report 404 if the item cannot be found in the list')
-  
+
   it('should fail with an appropiate message if the unit is incompatible')
 })
