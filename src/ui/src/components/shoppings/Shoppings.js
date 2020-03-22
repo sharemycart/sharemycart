980112c492e2e173a064d409cbd7c3cdc422e0c4
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2020-03-22 05:21:29 +0000

    Feature: Providing the shoppings list, now all we need is to wire this with the db.

--- src/ui/src/components/shoppings/Shoppings.js
+++ src/ui/src/components/shoppings/Shoppings.js
@@ -1,16 +1,73 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-
 import {
-	IonContent
+	IonContent,
+	IonIcon,
+	IonBadge,
+	IonLabel,
+	IonButton,
+	IonItem,
+	IonList,
+	IonSearchbar,
 } from '@ionic/react';
+import { trash, cart, add } from 'ionicons/icons';
 
 // MOBX
 import { inject, observer } from 'mobx-react';
 import TabContainer from '../tabs/TabContainer';
 import BasicPage from '../basicpage/BasicPage';
 
+const ENTER_KEY = 13;
+// @todo: load this from the server
+const items = [{
+	id: 1,
+	name: 'Tomatoes',
+	unit: 'kg',
+	quantity: 5
+}, {
+	id: 2,
+	name: 'Avocado',
+	unit: 'pc',
+	quantity: 1
+}, {
+	id: 3,
+	name: 'Flour',
+	unit: 'g',
+	quantity: 500
+}];
+
 class Shoppings extends Component {
+
+	constructor (props) {
+		super(props);
+		this.state = { searchText: '', items: items };
+	}
+
+	onAddItem = (evt) => {
+		if (evt.which !== ENTER_KEY) {
+			return;
+		}
+		let text = evt.target.value;
+		console.log(text);
+		let newItems = this.state.items;
+		newItems.push({
+			id: 123,
+			name: text
+		});
+		this.setState({
+			items: newItems
+		});
+		// @todo: connect here
+		// this.props.store.addItem({ text: text })
+		// 	.then(() => {
+		// 		// clear the search input
+		// 		this.setState({ searchText: '' });
+		// 		// @todo: reload the store
+		// 	}).catch(err => {
+		// 	console.error(err);
+		// 	// @todo: handle exception here with a toast
+		// });
+	};
 
 	render () {
 		return (
@@ -19,9 +76,29 @@ class Shoppings extends Component {
 				store={this.props.store}
 				renderContent={history => {
 					return (
-						<IonContent>
-							<TabContainer history={this.props.history}/>
-						</IonContent>
+						<>
+							<IonContent>
+								<TabContainer history={this.props.history}/>
+								<IonSearchbar
+									searchIcon={cart}
+									value={this.state.searchText}
+									onKeyPress={this.onAddItem}
+									placeholder="Type to add new products"></IonSearchbar>
+								<IonList>
+									{this.state.items.map(item => {
+										// @todo: add the handlers for the buttons plus and remove
+										return (<IonItem key={item.id}>
+											<IonLabel>{item.name}</IonLabel>
+											<IonBadge slot="end" color="dark">{item.quantity}{item.unit}</IonBadge>
+											<IonButton fill="add" size="large" slot="end" color="primary"><IonIcon
+												icon={add}/></IonButton>
+											<IonButton className="button-end" fill="clear" size="large" slot="end" color="danger"><IonIcon
+												icon={trash}/></IonButton>
+										</IonItem>);
+									})}
+								</IonList>
+							</IonContent>
+						</>
 					);
 				}}
 			>
