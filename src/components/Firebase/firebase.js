add7f7993c419e26c2357285f5a1c01e732d8dde
mrsimpson <github@beimir.net>
2020-03-28 09:20:51 +0100

    Start off from rwieruch's boilerplate

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,12 +3,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  apiKey: process.env.REACT_APP_BACKEND_API_KEY,
+  authDomain: process.env.REACT_APP_BACKEND_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_BACKEND_DATABASE_URL,
+  projectId: process.env.REACT_APP_BACKEND_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_BACKEND_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_BACKEND_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_BACKEND_SENDER_APP_ID
 };
 
 class Firebase {
