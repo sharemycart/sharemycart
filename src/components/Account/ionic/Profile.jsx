5be7e607bb26dc58dcd2b005908e9d157261a87d
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2020-04-13 12:12:33 +0200

    Fixes card styling (#72)
    * Fix: Moving away the Avatar Image to a reusable component that can be centered, the Avatar Component is very specific to the lists.
    
    * Fix: Fixed the user account view layout as well.
    
    * Remove unused var
    
    Co-authored-by: mrsimpson <github@beimir.net>

--- src/components/Account/ionic/Profile.jsx
+++ src/components/Account/ionic/Profile.jsx
@@ -2,34 +2,20 @@ import React from 'react';
 import { compose } from 'recompose';
 import { inject, observer } from 'mobx-react';
 import { IonCardHeader, IonCard, IonCardSubtitle, IonCardTitle } from '@ionic/react';
-import Avatar from '../../Reusables/ionic/Avatar';
+import UserProfileImg from '../../Reusables/ionic/UserProfileImg';
 
 const Profile = (props) => {
     const user = props.sessionStore.authUser;
     return (
-        <div style={{
-            height: "300px",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
+        <IonCard style={{
+            padding: '30px',
         }}>
-            <IonCard style={{
-                padding: '30px',
-            }}>
-            <div>
-                <Avatar
-                    user={user}
-                    size="200px"
-                />
-                </div>
-                <div>
-                <IonCardHeader class="ion-text-center">
-                    <IonCardSubtitle >{user.username}</IonCardSubtitle>
-                    <IonCardTitle>{user.email}</IonCardTitle>
-                </IonCardHeader>
-                </div>
-            </IonCard>
-        </div>
+            <IonCardHeader class="ion-text-center">
+                <UserProfileImg user={user} size={200} />
+                <IonCardSubtitle >{user.username}</IonCardSubtitle>
+                <IonCardTitle>{user.email}</IonCardTitle>
+            </IonCardHeader>
+        </IonCard>
     )
 }
 
