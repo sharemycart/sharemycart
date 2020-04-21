1c6af48f9d5a636d884e8e77672465007fd60677
mrsimpson <github@beimir.net>
2020-04-21 22:51:42 +0200

    WIP: pass config

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,5 +3,14 @@ const cypressFirebasePlugin = require("cypress-firebase").plugin;
 
 module.exports = (on, config) => {
   // Pass on function, config, and admin instance. Returns extended config
-  return cypressFirebasePlugin(on, config, admin);
+ cypressFirebasePlugin(on, config, admin);
+
+    // we can grab some process environment variables
+  // and stick it into config.env before returning the updated config
+  config.env = process.env || {}
+  console.log('extended config.env with process.env.', JSON.stringify(process.env))
+
+  return config
+
+
 };
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,9 +4,19 @@ import "firebase/database";
 import "firebase/firestore";
 import { attachCustomCommands } from "cypress-firebase";
 
-const fbConfig = {
-  };
+const projectId = Cypress.env('REACT_APP_BACKEND_PROJECT_ID')
+const env = Cypress.env('env') || 'stage'
+const apiKey = Cypress.env('REACT_APP_BACKEND_API_KEY')
 
+const fbConfig = {
+  apiKey,
+  authDomain: `${projectId}.firebaseapp.com`,
+  databaseURL: `https://${projectId}.firebaseio.com`,
+  projectId: `${projectId}`,
+  storageBucket: `${projectId}.appspot.com`
+}
+
+console.log('fbConfig', fbConfig)
 firebase.initializeApp(fbConfig);
 
 attachCustomCommands({ Cypress, cy, firebase }); 
