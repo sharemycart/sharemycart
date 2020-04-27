5555d0fbbf9a616bb75a7d276ddb2a40d5db86a5
mrsimpson <github@beimir.net>
2020-04-27 08:52:37 +0200

    Add linter config

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:cypress/recommended",
+        "plugin:chai-friendly/recommended",
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            // "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
