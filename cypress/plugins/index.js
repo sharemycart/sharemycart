ed37b94248af80e805166ce1ada3cfb637f2c853
mrsimpson <github@beimir.net>
2020-04-29 16:00:22 +0200

    1

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -8,6 +8,12 @@ module.exports = (on, config) => {
 	require('@cypress/code-coverage/task')(on, config)
 
 	
+	// eslint-disable-next-line no-console
+	console.log(JSON.stringify({
+		REACT_APP_BACKEND_PROJECT_ID: process.env.REACT_APP_BACKEND_PROJECT_ID,
+		REACT_APP_BACKEND_API_KEY: process.env.REACT_APP_BACKEND_API_KEY,
+		TEST_UID: process.env.TEST_UID,
+	}))
 	// Extend the cypress configuration
 	return Object.assign(config,
 		// functions for interacting with Firestore
