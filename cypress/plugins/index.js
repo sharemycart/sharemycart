63ab7351b9ba8156b353955e860647d244e114f4
Oliver Jägle <mrsimpson@users.noreply.github.com>
2020-05-03 23:36:26 +0200

    Set up production II (#104)
    * Fix projectId in build
    
    * NPM target for building staging (btea) deliverable
    
    * Don't try to force node_env=test
    CRA does not allow to overwrite it
    
    * Don't run, just test
    
    * Make test env default for development
    
    * Use dedicated build targets to fix env-selection
    
    * Optimize pipeline
    
    * Tune pipeline
    
    * Make delay before typing in tests configurable

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -7,19 +7,27 @@ module.exports = (on, config) => {
 	// custom tasks for sending and reporting code coverage
 	require('@cypress/code-coverage/task')(on, config)
 
+	const env = {
+		REACT_APP_BACKEND_PROJECT_ID: process.env.REACT_APP_BACKEND_PROJECT_ID,
+		REACT_APP_BACKEND_API_KEY: process.env.REACT_APP_BACKEND_API_KEY,
+		CI: process.env.CI,
+		NODE_ENV: process.env.NODE_ENV,
+		TEST_UID: process.env.TEST_UID,
+	}
+
+	// copy all variables prefixed with CY_
+	for (const k in process.env) {
+		if (k.match(/^CY_/) && process.env.hasOwnProperty(k)) {
+			env[k] = parseInt(process.env[k]) || process.env[k] // convert to number if it's a stringified number
+		}
+	}
+
 	// Extend the cypress configuration
 	return Object.assign(config,
 		// functions for interacting with Firestore
 		cypressFirebasePlugin(on, config, admin),
-
 		// propagate ENV-variables
 		{
-			env: {
-				REACT_APP_BACKEND_PROJECT_ID: process.env.REACT_APP_BACKEND_PROJECT_ID,
-				REACT_APP_BACKEND_API_KEY: process.env.REACT_APP_BACKEND_API_KEY,
-				CI: process.env.CI,
-				NODE_ENV: process.env.NODE_ENV,
-				TEST_UID: process.env.TEST_UID,
-			}
+			env
 		})
 }
